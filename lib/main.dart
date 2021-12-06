ca288fa42fc962aea222be9a76ff97822cc3d0b5
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2021-12-06 18:55:11 +0600

    feat(BEGIN): Add MaterialApp and Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-      "SomeInfo",
-      textDirection: TextDirection.ltr,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold, color: Colors.purple, fontSize: 100),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text("WayHome"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            "HomePage",
+            style: TextStyle(
+                fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
