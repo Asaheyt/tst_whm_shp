797a04d384ca25f2874c93b556ece06464547d23
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2022-01-05 11:13:47 +0600

    feat(BEGIN): Added stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(First());
+
+class First extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.indigoAccent,
         appBar: AppBar(
           title: Text("WayHome"),
           centerTitle: true,
         ),
         body: Center(
-          child: Text(
-            "HomePage",
-            style: TextStyle(
-                fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinearProgressIndicator(
+                  value: 23,
+                ),
+                Text(
+                  "23 %",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Press Button to Download",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(
+            Icons.cloud_download,
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
