b68fb2d1d6f390da5cc2e552a5398de531fb8959
Adil Aldamzharov <a.aldamzharov@bytepride.dev>
2022-01-10 12:14:33 +0600

    feat(BEGIN): Added assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(First());
@@ -12,15 +10,6 @@ class First extends StatefulWidget {
 }
 
 class _FirstState extends State<First> {
-  late bool _loading;
-  late double _progressValue;
-
-  @override
-  void initState() {
-    _loading = false;
-    _progressValue = 0.0;
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,60 +21,17 @@ class _FirstState extends State<First> {
           centerTitle: true,
         ),
         body: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      LinearProgressIndicator(
-                        value: _progressValue,
-                      ),
-                      Text(
-                        "${(_progressValue * 100).round()}%",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  )
-                : Text(
-                    "Press Button to Download",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: Icon(
-            Icons.cloud_download,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image(
+                image: AssetImage('images/landscape.jpg'),
+              ),
+              Image.asset('icons/brBad.png')
+            ],
           ),
         ),
       ),
     );
-  }
-
-  void _updateProgress() {
-    const oneSec = const Duration(milliseconds: 500);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2;
-        if (_progressValue >= 1.0) {
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
-    });
   }
 }
